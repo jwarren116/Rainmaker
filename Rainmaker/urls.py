86f76c19d409b7146b225a391a5b82830896ed3b
James Warren <jwarren116@gmail.com>
2015-09-03 09:56:07 -0700

    Add empty URLs module to shop app; point Rainmaker/urls.py to new URLs module

--- Rainmaker/urls.py
+++ Rainmaker/urls.py
@@ -5,5 +5,6 @@ from Rainmaker import views
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^shop/', include('shop.urls')),
     url(r'^$', views.index, name='index'),
 )
