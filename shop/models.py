d798e6c821a9f6f8aaedafc907299cebc0580937
James Warren <jwarren116@gmail.com>
2015-09-03 11:27:32 -0700

    Add Product model

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=63)
+    description = models.CharField(max_length=255, blank=True)
+    photo = models.ImageField(upload_to='products')
+    price = models.DecimalField(decimal_places=2)
+    item_number = models.IntegerField()
