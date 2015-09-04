170e4814c459542265471f6bb1f3147b3726a390
James Warren <jwarren116@gmail.com>
2015-09-04 15:52:34 -0700

    Add admin for Cart model

--- shop/admin.py
+++ shop/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
-from shop.models import Product
+from shop.models import Product, Cart
 
 
 class ProductAdmin(admin.ModelAdmin):
+    model = Product
     list_display = ('name', 'description', 'price', 'photo', 'item_number')
 
+
+class CartAdmin(admin.ModelAdmin):
+    model = Cart
+
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Cart, CartAdmin)
