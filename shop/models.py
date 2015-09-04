e392d0b566bd98f355a354b9f36dfa6b15a3fec5
James Warren <jwarren116@gmail.com>
2015-09-04 15:52:10 -0700

    Add str method to Product model; add Cart model

--- shop/models.py
+++ shop/models.py
@@ -7,3 +7,13 @@ class Product(models.Model):
     photo = models.ImageField(upload_to='products')
     price = models.DecimalField(max_digits=9, decimal_places=2)
     item_number = models.IntegerField()
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.item_number)
+
+
+class Cart(models.Model):
+    item = models.ManyToManyField(Product, related_name='cart')
+
+    def __str__(self):
+        return str(self.item)
