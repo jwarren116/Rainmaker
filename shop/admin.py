ef6b876da9d59ed0f185efbb5f90eb8e43dcd542
James Warren <jwarren116@gmail.com>
2015-09-03 21:54:21 -0700

    Add basic admin for Product model

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from shop.models import Product
 
-# Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'photo', 'item_number')
+
+admin.site.register(Product, ProductAdmin)
