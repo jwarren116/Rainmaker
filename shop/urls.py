c9d1c5cbf477756db7483e012f93d67aca31caec
James Warren <jwarren116@gmail.com>
2015-09-04 18:36:16 -0700

    Add basic functions to handle storing and viewing cart from session cookie; add URL route for viewing cart

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from shop import views
 
 urlpatterns = patterns('',
     url(r'^$', views.shop, name='shop'),
+    url(r'^cart/$', views.view_cart, name='cart')
 )
--- shop/views.py
+++ shop/views.py
@@ -6,3 +6,19 @@ def shop(request):
     return render(request, 'shop.html', {
         'products': Product.objects.all(),
     })
+
+
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    return render(request, 'cart.html', {
+        'cart': cart,
+    })
+
+
+def add_to_cart(request, item_id, quantity):
+    cart = request.session.get('cart', {})
+    cart[item_id] = quantity
+    request.session['cart'] = cart
+    return render(request, 'cart.html', {
+        'cart': cart,
+    })
